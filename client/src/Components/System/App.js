194abb754a67452479cd5784a43a0e4ef5870020
Jose Villarroel <oscarclarke9900@gmail.com>
2021-02-01 00:57:52 +0100

    eslint fixes

--- client/src/Components/System/App.js
+++ client/src/Components/System/App.js
@@ -1,5 +1,28 @@
 import React from 'react'
+import { RecoilRoot } from 'recoil'
+import { ThemeProvider } from 'styled-components/macro'
+import { Router } from 'react-router-dom'
+import history from 'Config/history'
+import Routes from 'Components/System/routes'
+import { ErrorBoundary } from 'Components/atoms'
+import { GlobalStyle, theme } from 'Config/styled'
 
-const App = () => <div>hello</div>
+// TODO: style error boundary
+// TODO: react-i18next
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <ErrorBoundary fallback={<div>error</div>}>
+          <Router history={history}>
+            <Routes />
+          </Router>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </RecoilRoot>
+  </>
+)
 
 export default App
