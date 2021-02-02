d711c4609890edb60ab079b9af576d8d7e70c2ca
Jose Villarroel <oscarclarke9900@gmail.com>
2021-02-02 01:03:46 +0100

    some backend changes

--- backend/src/routes/characters.routes.ts
+++ backend/src/routes/characters.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { getCharacters, getCharacter } from '../controllers/characters.controller'
+import { TokenMiddleware } from '../libs/tokenMiddleware'
 import urls from '../constants/urls'
 
 const router = Router()
 
-router.get(urls.Base, getCharacters)
+router.get(urls.Base, TokenMiddleware, getCharacters)
 
-router.get(urls.Identifier, getCharacter)
+router.get(urls.Identifier, TokenMiddleware, getCharacter)
 
 export default router
